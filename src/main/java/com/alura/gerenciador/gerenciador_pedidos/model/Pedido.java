4e49d8538a7f96cf91aa45cf8c2108a523809ecb
Vinicius Saes <viniciusaesouza@gmail.com>
2025-03-12 17:07:28 -0300

    feat: conexao com bd + variaves locais

--- src/main/java/com/alura/gerenciador/gerenciador_pedidos/model/Pedido.java
+++ src/main/java/com/alura/gerenciador/gerenciador_pedidos/model/Pedido.java
@@ -6,7 +6,7 @@ import java.time.LocalDate;
 @Entity
 public class Pedido {
     @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDate data;
 
